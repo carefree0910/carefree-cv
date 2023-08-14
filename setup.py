713c30c2506c9142822fbc24e815e777c02f61ae
carefree0910 <syameimaru.saki@gmail.com>
2023-08-14 10:55:31 +0800

    📦️Upgraded `VERSION`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 DESCRIPTION = "Some commonly used computer vision functions and modules"
 with open("README.md") as f:
